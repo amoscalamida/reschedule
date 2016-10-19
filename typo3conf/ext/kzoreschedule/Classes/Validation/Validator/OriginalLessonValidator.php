f69551b5246392d21cd057ac81b629e9b207b0db
amoscalamida <amos.calamida@me.com>
2016-10-19 12:07:42 +0200

    Removing annotation for unfinished ChangeValidator

--- typo3conf/ext/kzoreschedule/Classes/Validation/Validator/OriginalLessonValidator.php
+++ typo3conf/ext/kzoreschedule/Classes/Validation/Validator/OriginalLessonValidator.php
@@ -1,17 +1,17 @@
 <?php
-namespace \AmosCalamida\Kzoreschedule\Validation\Validator;
+namespace AmosCalamida\Kzoreschedule\Validation\Validator;
 
 class OriginalLessonValidator extends \TYPO3\CMS\Extbase\Validation\Validator\AbstractValidator
 {
     public function isValid($originalLesson)
     {
 
-        if (date('U',strtotime($originalLesson)) <= date('U',strtotime('tomorrow'))){
+        if (date('U', strtotime($originalLesson)) <= date('U', strtotime('tomorrow'))) {
             $this->addError('changedLesson Date is older than tomorrow.', 1262341491);
             return FALSE;
         }
 
-        if (!in_array(date("H:i",strtotime($originalLesson)), array('07:30','08:25','09:20','10:25','11:20','12:25','13:20','14:05','15:10','16:05','16:55'))) {
+        if (!in_array(date("H:i", strtotime($originalLesson)), array('07:30', '08:25', '09:20', '10:25', '11:20', '12:25', '13:20', '14:05', '15:10', '16:05', '16:55'))) {
             $this->addError('changedLesson Time is not a valid lesson beginning.', 1262341492);
             return FALSE;
         }
