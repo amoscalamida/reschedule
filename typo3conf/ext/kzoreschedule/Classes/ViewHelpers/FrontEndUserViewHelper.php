407b3e78acddc9d11440f483b780a9f95c718e2f
amoscalamida <amos.calamida@me.com>
2016-11-07 17:03:21 +0100

    Added FE User ViewHelper

---
+++ typo3conf/ext/kzoreschedule/Classes/ViewHelpers/FrontEndUserViewHelper.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AmosCalamida\Kzoreschedule\ViewHelpers;
+
+
+class FrontEndUserViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
+{
+    /**
+     * this ViewHelper searches infromation for the current FE User
+     *
+     * @param string $attribute - The attribute to get for the FE User
+     * @return mixed - the value of the attribut
+     */
+
+    public function render($attribute)
+    {
+
+        $value = $GLOBALS['TSFE']->fe_user->user[$attribute];
+
+        if (!empty($value)) {
+            return $value;
+        }
+        else {
+            return "Error";
+        }
+
+
+    }
+}
+
+?>
